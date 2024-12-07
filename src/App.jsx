f43f34324f55c76e69a882466f6ec37df20224a0
Tiffany Ho <tiffanieho@outlook.com>
2024-12-07 14:56:01 +0000

    feat: add state for flight option selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const App = () => {
+  const [flightOption, setFlightOption] = useState('one-way');
   return (
     <div>
       <form className="flight-booker">
-        <select>
+        <select value={flightOption}
+          onChange={(event) = setFlightOption(event.target.value)}
+        >
           <option value="one-way">One-way flight</option>
           <option value="return">Return flight</option>
         </select>
