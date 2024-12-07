59587cefec7a811e93af11109c9dfa688a186634
Tiffany Ho <tiffanieho@outlook.com>
2024-12-07 15:02:17 +0000

    feat: add conditional rendering for return date input

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ const App = () => {
   const TODAY = formatDate(new Date());
   const [departureDate, setDepartureDate] = useState(TODAY);
   const [flightOption, setFlightOption] = useState("one-way");
-
+  const [returnDate, setReturnDate] = useState(departureDate);
   //handle date formating
   function formatDate(date) {
     const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
   }
-  
+
   return (
     <div>
       <form className="flight-booker">
@@ -29,6 +29,14 @@ const App = () => {
           onChange={(event) => setDepartureDate(event.target.value)}
           min={TODAY}
         />
+        {flightOption === "return" && (
+          <input
+            type="date"
+            value={returnDate}
+            min={departureDate}
+            onChange={(event) => setReturnDate(event.target.value)}
+          />
+        )}
         <button>Book</button>
       </form>
     </div>
