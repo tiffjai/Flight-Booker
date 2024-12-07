070aa849180f5b9a5d8a774b8bde84f540dd0496
Tiffany Ho <tiffanieho@outlook.com>
2024-12-07 14:59:45 +0000

    feat: add departure date input with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  const [flightOption, setFlightOption] = useState('one-way');
+  const TODAY = formatDate(new Date());
+  const [departureDate, setDepartureDate] = useState(TODAY);
+  const [flightOption, setFlightOption] = useState("one-way");
   return (
     <div>
       <form className="flight-booker">
-        <select value={flightOption}
-          onChange={(event) = setFlightOption(event.target.value)}
+        <select
+          value={flightOption}
+          onChange={(event = setFlightOption(event.target.value))}
         >
           <option value="one-way">One-way flight</option>
           <option value="return">Return flight</option>
         </select>
-        <button>
-          Book
-        </button>
+        <input
+          type="date"
+          value={departureDate}
+          onChange={(event) => setDepartureDate(event.target.value)}
+          min={TODAY}
+        />
+        <button>Book</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
